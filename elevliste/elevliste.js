e364a12fa31894dc4a975cfe90eb6ea8061ad50a
Audun Hauge <audun.hauge@gmail.com>
2020-09-02 10:20:21 +0200

    elev registrering

--- elevliste/elevliste.js
+++ elevliste/elevliste.js
@@ -1,19 +1,28 @@
 // @ts-check
 
-const elevListe = [
-    "ole",
-    "petter",
-    "kari",
-    "lise",
-    "sjur",
-    "anne"
-];
+const elevListe = [ ];
 
 function setup() {
     let divElever = document.getElementById("elever");
     let divFunnet = document.getElementById("funnet");
+    let inpNavn = document.getElementById("navn");
+
+    let btnLagre = document.getElementById("lagre");
+    btnLagre.addEventListener("click",lagreElev);
+
     let btnVis = document.getElementById("vis");
     btnVis.addEventListener("click", visElever);
+
+    function lagreElev() {
+        // @ts-ignore
+        let navn = inpNavn.value;
+        if (navn === "") {
+            alert("Skriv ett gyldig navn");
+            return;
+        }
+        elevListe.push(navn);
+        visElever();
+    }
 
     function visElever() {
         let s = "";
@@ -21,8 +30,6 @@ function setup() {
         for (let i=0; i<antall; i += 1) {
             s += elevListe[i] + "<br>";
         }
-
-
         divElever.innerHTML = s;
     }
 }
