40725ec0e6efec644e0c74dc51136aadfa0e7867
Audun Hauge <audun.hauge@gmail.com>
2020-09-09 11:30:42 +0200

    mange

--- ballspill/ballspill.js
+++ ballspill/ballspill.js
@@ -1,6 +1,7 @@
 // @ts-check
 
 
+
 /**
  * @file Et ballspill.<br>
  * Denne kommentaren vises på første side
@@ -18,8 +19,8 @@
 class Ball {
     x=0;
     y=0;
-    w=0;
-    h=0;
+    w=20;
+    h=20;
     vx=0;
     vy=0;
     div = undefined;
@@ -34,30 +35,68 @@ class Ball {
 }
 
 /**
+ * ballTabell inneholder alle ballene i spillet
+ */
+const ballTabell = [ ];
+const ANTALL = 20;
+
+function rndColor() {
+    const colors = "red,green,blue,yellow,pink,teal,aqua,orange".split(",");
+    const antall = colors.length;
+    const indeks = Math.trunc( Math.random() * antall);
+    return colors[indeks];
+}
+
+
+/**
  * Oppstartsfunksjonen
  * Lager en ball og starter en timer
  */
 function setup() {
     const divBrett = document.getElementById("brett");
     const divStatus = document.getElementById("status");
-    const b = new Ball();
-    b.x = 100;
-    b.y = 100;
-    b.h = 20;
-    b.w = 20;
-    b.vx = 5;
-    b.vy = 0;
-    b.div = document.createElement("div");
-    divBrett.append(b.div);
-    b.div.className = "ball";
+    const maxX = Math.random() * 7 + 4;
+    const maxY = Math.random() * 7 + 4;
 
-    b.tegnPaaSkjerm();
+    for(let i=0; i<ANTALL; i += 1) {
+        const b = new Ball();
+        b.x = Math.random() * 460 + 20;
+        b.y = Math.random() * 460 + 20;
+        b.vx = 5;
+        b.vy = -4;
+        b.div = document.createElement("div");
+        divBrett.append(b.div);
+        b.div.className = "ball";
+        b.div.style.backgroundColor = rndColor();
+
+        b.tegnPaaSkjerm();
+        ballTabell.push(b);
+    }
+
+    
 
     setInterval(flyttPaaBallen,20);
 
     function flyttPaaBallen() {
-        b.x += 5;
-        b.tegnPaaSkjerm();
+        for (let i=0; i<ANTALL; i += 1) {
+            const b = ballTabell[i];
+            b.x += b.vx;
+            b.y += b.vy;
+            if (b.x > 500 - b.w) {
+                b.vx = -maxX;
+            }
+            if (b.x < 0) {
+                b.vx = maxX;
+            }
+            if (b.y > 500 - b.h) {
+                b.vy = -maxY;
+            }
+            if (b.y < 0) {
+                b.vy = maxY;
+            }
+            b.tegnPaaSkjerm();
+        }
+       
     }
 
 }
