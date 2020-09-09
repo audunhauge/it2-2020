3e797bf6cc03c0f659df99c0f0bbe7b9058999e5
Audun Hauge <audun.hauge@gmail.com>
2020-09-09 09:42:43 +0200

    jsdoc

--- ballspill/ballspill.js
+++ ballspill/ballspill.js
@@ -1,5 +1,20 @@
 // @ts-check
 
+
+/**
+ * @file Et ballspill.<br>
+ * Denne kommentaren vises på første side
+ * av dokumentasjonen - pga @ f i l e
+ * <p>
+ * Dette spillet har en ball
+ * </p>
+ */
+
+/**
+ * Enkel klasse for å plassere en ball
+ * på skjermen. (x,y) er posisjon
+ * (vx,vy) er fart, w,h er width,height
+ */
 class Ball {
     x=0;
     y=0;
@@ -8,12 +23,20 @@ class Ball {
     vx=0;
     vy=0;
     div = undefined;
+
+    /**
+     * Oppdaterer posisjon til div på skjermen
+     */
     tegnPaaSkjerm() {
         this.div.style.left = this.x + "px";
         this.div.style.top = this.y + "px";
     }
 }
 
+/**
+ * Oppstartsfunksjonen
+ * Lager en ball og starter en timer
+ */
 function setup() {
     const divBrett = document.getElementById("brett");
     const divStatus = document.getElementById("status");
