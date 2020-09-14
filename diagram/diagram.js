b4df58a7e8ec0704aec67ace75c5fea4e386d85f
Audun Hauge <audun.hauge@gmail.com>
2020-09-14 08:41:50 +0200

    diagram

---
+++ diagram/diagram.js
@@ -0,0 +1,19 @@
+// @ts-check
+
+
+function setup() {
+    const inpTallverdi = document.getElementById("tallverdi");
+    const btnLagre =document.getElementById("lagre");
+    const btnTegn = document.getElementById("tegn");
+
+    btnLagre.addEventListener("click", lagreData);
+    btnTegn.addEventListener("click",tegnSoyler);
+
+    function lagreData() {
+
+    }
+
+    function tegnSoyler() {
+
+    }
+}
