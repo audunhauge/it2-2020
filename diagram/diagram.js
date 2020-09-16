c2acfb5cd2d6742009927403fcb7a462db85f111
Audun Hauge <audun.hauge@gmail.com>
2020-09-16 10:14:57 +0200

    finn medianen

--- diagram/diagram.js
+++ diagram/diagram.js
@@ -19,8 +19,17 @@ function setup() {
         inpTallverdi.value = "";
         inpTallverdi.focus();
         divTalldata.innerHTML = String(tallListe);
-        divTalldata.innerHTML += "<br> Summen er " + summer(tallListe);
-        divTalldata.innerHTML += "<br> Max er " + maximum(tallListe);
+        const max = maximum(tallListe);
+        const sum = summer(tallListe);
+        const min = minimum(tallListe);
+        const snitt = gjennomsnitt(tallListe);
+        const med = median(tallListe);
+        divTalldata.innerHTML += `<br> Summen er ${sum}` ;
+        divTalldata.innerHTML += `<br> Max er ${max}` ;
+        divTalldata.innerHTML += `<br> Min er ${min}`;
+        const spenn = max - min;
+        divTalldata.innerHTML += `<br> Dynamisk spenn er ${spenn}`;
+        divTalldata.innerHTML += `<br> Gjennomsnitt er ${snitt}`;
     }
 
     function tegnSoyler() {
@@ -29,24 +38,56 @@ function setup() {
 }
 
 /**
- * Finner det største tallet i en tabell (array)
- * @param {Number[]} enArray - en liste med tall
- * @returns {Number}
+ * Beregner gjennomsnitt av tallverdier i en tabell
+ * @param {number[]} tabell
+ * @returns {number|undefined}
  */
-function maximum(enArray) {
-   let m = enArray[0];  // antar at dette er størst
-   for (let i=0; i < enArray.length; i += 1) {
-       const t = enArray[i];
-       if (m > t) m = t;
-   }
-   return m;
+function gjennomsnitt(tabell) {
+    if (tabell.length === 0) {
+        return undefined;
+    }
+    const sum = summer(tabell);
+    return sum/tabell.length;
+}
+
+
+/**
+ * Finner det største tallet i en tabell
+ * @param {number[]} tabell
+ * @returns {number}
+ */
+function minimum(tabell) {
+    let min = tabell[0];
+    for (let i=1; i < tabell.length; i += 1) {
+       const tall = tabell[i];
+       if (tall < min) {
+           min = tall;
+       }
+     }
+    return min;
 }
 
 /**
- * Beregner summen av en array
- * summer([1,2,3]) === 1+2+3
- * @param {number[]} tallSerie
- * @returns {Number}
+ * Finner det største tallet i en tabell
+ * @param {number[]} tabell
+ * @returns {number}
+ */
+function maximum(tabell) {
+    let max = tabell[0];
+    for (let i=1; i < tabell.length; i += 1) {
+       const tall = tabell[i];
+       if (tall > max) {
+           max = tall;
+       }
+     }
+    return max;
+}
+
+
+/**
+ * Summerer tallene i en array
+ * @param {string | any[]} tallSerie
+ * @returns {number}
  */
 function summer(tallSerie) {
     let sum = 0;
