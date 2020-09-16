a506abfa6ce3cd23a31962f8e8bcc4a3e592d719
Audun Hauge <audun.hauge@gmail.com>
2020-09-16 11:30:13 +0200

    tegn diagram

--- diagram/diagram.js
+++ diagram/diagram.js
@@ -1,17 +1,18 @@
 // @ts-check
 
 
-const tallListe = [ ];
+const tallListe = [];
 
 function setup() {
     const inpTallverdi =  /**  @type {HTMLInputElement} */
         (document.getElementById("tallverdi"));
-    const btnLagre =document.getElementById("lagre");
+    const btnLagre = document.getElementById("lagre");
     const btnTegn = document.getElementById("tegn");
     const divTalldata = document.getElementById("talldata");
+    const divDiagram = document.getElementById("diagram");
 
     btnLagre.addEventListener("click", lagreData);
-    btnTegn.addEventListener("click",tegnSoyler);
+    btnTegn.addEventListener("click", tegnDiagram);
 
     function lagreData() {
         const tall = inpTallverdi.valueAsNumber;
@@ -24,16 +25,30 @@ function setup() {
         const min = minimum(tallListe);
         const snitt = gjennomsnitt(tallListe);
         const med = median(tallListe);
-        divTalldata.innerHTML += `<br> Summen er ${sum}` ;
-        divTalldata.innerHTML += `<br> Max er ${max}` ;
+        divTalldata.innerHTML += `<br> Summen er ${sum}`;
+        divTalldata.innerHTML += `<br> Max er ${max}`;
         divTalldata.innerHTML += `<br> Min er ${min}`;
         const spenn = max - min;
+        const dupl = duplikater(tallListe);
         divTalldata.innerHTML += `<br> Dynamisk spenn er ${spenn}`;
         divTalldata.innerHTML += `<br> Gjennomsnitt er ${snitt}`;
+        divTalldata.innerHTML += `<br> Duplikater er ${dupl}`;
     }
 
-    function tegnSoyler() {
+    function tegnDiagram() {
+        tegnSoyler(tallListe);
+    }
 
+    /**
+     * @param {number[]} tabell
+     * @returns {void}
+     */
+    function tegnSoyler(tabell) {
+        for (let i = 0; i < tabell.length; i += 1) {
+            const tall = tabell[i];
+            const divSoyle = document.createElement("div");
+            divSoyle.className = "soyle";
+        }
     }
 }
 
@@ -47,7 +62,7 @@ function gjennomsnitt(tabell) {
         return undefined;
     }
     const sum = summer(tabell);
-    return sum/tabell.length;
+    return sum / tabell.length;
 }
 
 
@@ -58,12 +73,12 @@ function gjennomsnitt(tabell) {
  */
 function minimum(tabell) {
     let min = tabell[0];
-    for (let i=1; i < tabell.length; i += 1) {
-       const tall = tabell[i];
-       if (tall < min) {
-           min = tall;
-       }
-     }
+    for (let i = 1; i < tabell.length; i += 1) {
+        const tall = tabell[i];
+        if (tall < min) {
+            min = tall;
+        }
+    }
     return min;
 }
 
@@ -74,12 +89,12 @@ function minimum(tabell) {
  */
 function maximum(tabell) {
     let max = tabell[0];
-    for (let i=1; i < tabell.length; i += 1) {
-       const tall = tabell[i];
-       if (tall > max) {
-           max = tall;
-       }
-     }
+    for (let i = 1; i < tabell.length; i += 1) {
+        const tall = tabell[i];
+        if (tall > max) {
+            max = tall;
+        }
+    }
     return max;
 }
 
@@ -91,9 +106,32 @@ function maximum(tabell) {
  */
 function summer(tallSerie) {
     let sum = 0;
-    for (let i=0; i < tallSerie.length; i += 1) {
-       const tall = tallSerie[i];
-       sum += tall;
+    for (let i = 0; i < tallSerie.length; i += 1) {
+        const tall = tallSerie[i];
+        sum += tall;
     }
     return sum;
+}
+
+
+/**
+ * Finner medianverdien for en tallrekke
+ * @param {number[]} tabell
+ * @returns {number|undefined}
+ */
+function median(tabell) {
+    const kopi = tabell.slice();
+    kopi.sort((a, b) => a - b);
+    const midten = Math.trunc(tabell.length / 2);
+    return kopi[midten];
+}
+
+
+/**
+ * @param {number[]} tabell
+ * @returns {number[]}
+ */
+function duplikater(tabell) {
+
+    return [];
 }
