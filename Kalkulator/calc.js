d23b87d01d9b9d450e25ece44341ae889685d85d
Audun Hauge <audun.hauge@gmail.com>
2020-10-28 10:45:45 +0100

    update

--- Kalkulator/calc.js
+++ Kalkulator/calc.js
@@ -3,14 +3,27 @@
 const $ = (id) => document.getElementById(id);
 
 function setup() {
-    const divTall = $("tall");
-    const symboler = "789456123-0.";
-    for (let i=0; i<12; i++) {
-        const div = document.createElement("div");
-        div.className = "button";
-        divTall.append(div);
-        div.innerHTML = symboler.charAt(i);
+    {
+        const divTall = $("tall");
+        const symboler = "789456123-0.";
+        for (let i = 0; i < 12; i++) {
+            const div = document.createElement("div");
+            div.className = "button";
+            divTall.append(div);
+            div.innerHTML = symboler.charAt(i);
+        }
+    }
+    {
+        const divOperator = $("operator");
+        const symboler = "+-*/";
+        for (let i = 0; i < 4; i++) {
+            const div = document.createElement("div");
+            div.className = "button";
+            divOperator.append(div);
+            div.innerHTML = symboler.charAt(i);
+        }
     }
     
+
 
 }
