a5ed4d3ad0849ef0155ec86402d04815a31d41d7
Audun Hauge <audun.hauge@gmail.com>
2020-11-04 11:35:02 +0100

    kalkupdate

--- Kalkulator/calc.js
+++ Kalkulator/calc.js
@@ -7,31 +7,133 @@ function setup() {
     let nyttTall = true;
     let minne = 0;
     let operator = "";
+    let trigMode = "deg";
 
     const divDisplay = $("display");
     const divTall = $("tall");
     const divAC = $("ac");
     const divOperator = $("operator");
     const divErlik = $("erlik");
+    const divFunk = $("funk");
+    const divSpecial = $("specials");
+    const divMode = $("mode");
+
+    function visMode() {
+        divMode.innerHTML = `${trigMode}`;
+    }
+
+    visMode();
 
     divOperator.addEventListener("click", opererTall);
     divErlik.addEventListener("click", doOperation);
+    divFunk.addEventListener("click", matteFunk);
+    divSpecial.addEventListener("click", specialFunk);
 
     divTall.addEventListener("click", sjekkTall);
     divAC.addEventListener("click", nullstill);
 
-    function doOperation(e) {
-        if (operator === "+") {
-            const svar = minne + Number(divDisplay.innerHTML);
-            divDisplay.innerHTML = String(svar);
-            minne = svar;
+    function specialFunk(e) {
+        const t = e.target;
+        if (t.className === "button") {
+            const funknavn = t.innerHTML;
+            if ("degrad".includes(funknavn)) {
+                switch (funknavn) {
+                    case "degrad":
+                        {
+                            if (trigMode === "rad") {
+                                trigMode = "deg";
+                            } else {
+                                trigMode = "rad";
+                            }
+                            break;
+                        }
+                }
+                visMode();
+            } else {
+                alert("funksjon " + funknavn + " mangler");
+            }
+        }
+    }
+
+    function matteFunk(e) {
+        const t = e.target;
+        if (t.className === "button") {
             nyttTall = true;
+            const funknavn = t.innerHTML;
+            if ("sincostan".includes(funknavn)) {
+                let tall = Number(divDisplay.innerHTML);
+                if (trigMode === "deg") {
+                    tall = Math.PI * tall / 180;
+                }
+                switch (funknavn) {
+                    case "sin":
+                        {
+                            const resultat = Math.sin(tall);
+                            divDisplay.innerHTML = resultat.toFixed(8);
+                            break;
+                        }
+                    case "cos":
+                        {
+                            const resultat = Math.cos(tall);
+                            divDisplay.innerHTML = String(resultat);
+                            break;
+                        }
+                    case "tan":
+                        {
+                            const resultat = Math.tan(tall);
+                            divDisplay.innerHTML = String(resultat);
+                            break;
+                        }
+                }
+            } else {
+                alert("funksjon " + funknavn + " mangler");
+            }
+        }
+    }
+
+    function doOperation(e) {
+        switch (operator) {
+            case "+":
+                {
+                    const svar = minne + Number(divDisplay.innerHTML);
+                    divDisplay.innerHTML = String(svar);
+                    minne = svar;
+                    nyttTall = true;
+                    break;
+                }
+            case "-":
+                {
+                    const svar = minne - Number(divDisplay.innerHTML);
+                    divDisplay.innerHTML = String(svar);
+                    minne = svar;
+                    nyttTall = true;
+                    break;
+                }
+            case "*":
+                {
+                    const svar = minne * Number(divDisplay.innerHTML);
+                    divDisplay.innerHTML = String(svar);
+                    minne = svar;
+                    nyttTall = true;
+                    break;
+                }
+            case "/":
+                {
+                    const svar = minne / Number(divDisplay.innerHTML);
+                    divDisplay.innerHTML = String(svar);
+                    minne = svar;
+                    nyttTall = true;
+                    break;
+                }
         }
     }
 
     function opererTall(e) {
         const t = e.target;
-        if (t.className = "button") {
+        if (t.className === "button") {
+            if (operator !== "") {
+                doOperation(null);
+            }
             operator = t.innerHTML;
             minne = Number(divDisplay.innerHTML);
             nyttTall = true;
@@ -45,7 +147,7 @@ function setup() {
 
     function sjekkTall(e) {
         const t = e.target;
-        if (t.className = "button") {
+        if (t.className === "button") {
             if (nyttTall) {
                 minne = Number(divDisplay.innerHTML);
                 divDisplay.innerHTML = "";
@@ -59,6 +161,7 @@ function setup() {
     lageKnapper("tall", "789456123-0.".split(""));
     lageKnapper("operator", "+-*/".split(""));
     lageKnapper("funk", "sin,cos,tan".split(","));
+    lageKnapper("specials", "degrad".split(","));
 
     function lageKnapper(id, symboler) {
         const divRamme = $(id);
@@ -69,7 +172,5 @@ function setup() {
             div.innerHTML = symboler[i];
         }
     }
-
-
 
 }
