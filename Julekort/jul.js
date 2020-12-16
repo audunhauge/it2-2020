637858159d89c2c5a08c51fa5381840c60de0cc3
Audun Hauge <audun.hauge@gmail.com>
2020-12-16 10:17:25 +0100

    gryla

--- Julekort/jul.js
+++ Julekort/jul.js
@@ -24,6 +24,7 @@ class Nisse {
 
 class Gran extends Nisse {
     vy = 0;
+    aktiv = false;
     constructor(divParent) {
         super();
         this.y = 500;
@@ -35,20 +36,59 @@ class Gran extends Nisse {
 
     }
 
+    skyt() {
+        this.aktiv = true;
+        this.vy = -20;
+    }
+
 }
 
 const skog = [];
 
 
 function setup() {
+    const FART = 15;
     const divSky = document.getElementById("sky");
+    addEventListener("keydown", styrSpillet);
     const nisse = new Nisse();
+    const gryla = new Nisse();
+    gryla.x = 600;
+    gryla.y = 480;
     for (let i=0; i<20; i++) {
         const gran = new Gran(divSky);
         skog.push(gran);
         gran.render();
     }
     nisse.div = document.getElementById("santa");
+    gryla.div = document.getElementById("gryla");
+
+    function styrSpillet(e) {
+        switch(e.key) {
+            case "ArrowLeft": {
+                gryla.x -= FART;
+            } break;
+            case "ArrowRight": {
+                gryla.x += FART;
+            } break;
+            case " ": {
+                for (let tre of skog) {
+                    const avstand = Math.abs(gryla.x - tre.x);
+                    if (avstand < 150) {
+                        tre.skyt();
+                        break;
+                    }
+                }
+                /* for alle juletrær
+                      med et valgt juletre
+                      regn ut avstand til gryla
+                      dersom avstand <  20
+                         fyr av dette juletreet
+                         break
+                      */
+            } break;
+        }
+        gryla.render();
+    }
 
     setInterval(() => {
         nisse.x += nisse.vx;
@@ -60,6 +100,18 @@ function setup() {
         if (nisse.x > 1050) {
             nisse.vx = -5;
         }
+        /*
+        travaser alle trær i skogen
+          dersom tre er aktivt
+             flytt treet oppover med vy
+        */
+       for (let tre of skog) {
+        if (tre.aktiv) {
+            tre.y += tre.vy;
+            tre.render();
+        }
+    }
+
 
     }, 50);
 
