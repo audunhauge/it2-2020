1e073f3a4536be9c34f159f5c706f0770b918f20
audun hauge <audun.hauge@gmail.com>
2021-01-04 08:50:20 +0100

    position

--- Canvas/canvas.js
+++ Canvas/canvas.js
@@ -6,8 +6,34 @@ function setup() {
   const π = Math.PI; // kjekk å ha
 
   ctx.beginPath();
+  ctx.strokeStyle = "green"
+  ctx.fillStyle = "blue";
   ctx.fillStyle = "rgb(200,0,0)";
   ctx.arc(100, 100, 30, 0, 2 * π);
   ctx.stroke();
   // tegner en sirkel
+
+  // tegner en firkant
+  ctx.fillRect(25, 25, 100, 100);
+  ctx.clearRect(45, 45, 60, 60);
+  ctx.strokeRect(50, 50, 50, 50);
+
+  // tegner en trekant
+  ctx.beginPath();
+  ctx.moveTo(275, 250);
+  ctx.lineTo(300, 275);
+  ctx.lineTo(300, 225);
+  ctx.fill();
+
+  // cursor position javascript
+  /*
+  var cX = event.clientX;
+  var sX = event.screenX;
+  var cY = event.clientY;
+  var sY = event.screenY;
+  var coords1 = "client - X: " + cX + ", Y coords: " + cY;
+  var coords2 = "screen - X: " + sX + ", Y coords: " + sY;
+  */
+
+
 }
