0fc9e497f306c43656745f54c15d5fb99dd0ad34
Audun Hauge <audun.hauge@gmail.com>
2021-01-06 11:03:01 +0100

    trekanter

--- Canvas/canvas.js
+++ Canvas/canvas.js
@@ -1,29 +1,114 @@
 // @ts-check
 
-function setup() {
-  let ctx = document.getElementById("tegning").getContext("2d");
-  // kobling til tegnings-kontekst for canvas ctx = context
-  const π = Math.PI; // kjekk å ha
+const π = Math.PI; // kjekk å ha
 
-  ctx.beginPath();
-  ctx.strokeStyle = "green"
-  ctx.fillStyle = "blue";
-  ctx.fillStyle = "rgb(200,0,0)";
-  ctx.arc(100, 100, 30, 0, 2 * π);
-  ctx.stroke();
+/**
+ * Beregner avstand mellom to punkt a og b
+ * @param {{ x: number; y: number; }} a
+ * @param {{ x: number; y: number; }} b
+ */
+function dist(a, b) {
+  const dx = a.x - b.x;
+  const dy = a.y - b.y;
+  return Math.sqrt(dx * dx + dy * dy)
+}
+
+/**
+ * @param {CanvasRenderingContext2D} ctx
+ * @param {{ x: any; y: any; }} p
+ * @param {number} r
+ */
+function sirkel(ctx, p, r) {
   // tegner en sirkel
-
-  // tegner en firkant
-  ctx.fillRect(25, 25, 100, 100);
-  ctx.clearRect(45, 45, 60, 60);
-  ctx.strokeRect(50, 50, 50, 50);
-
-  // tegner en trekant
   ctx.beginPath();
-  ctx.moveTo(275, 250);
-  ctx.lineTo(300, 275);
-  ctx.lineTo(300, 225);
-  ctx.fill();
+  ctx.arc(p.x, p.y, r, 0, 2 * π);
+  ctx.stroke();
+}
+
+/**
+ * Tegn en firkant
+ * @param {CanvasRenderingContext2D} ctx
+ * @param {{ x: any; y: any; }} p1
+ * @param {{ x: any; y: any; }} p2
+ */
+function firkant(ctx, p1, p2) {
+  // tegner en firkant
+  const w = p2.x - p1.x;
+  const h = p2.y - p1.y;
+  ctx.beginPath();
+  ctx.strokeRect(p1.x, p1.y, w, h);
+  //ctx.stroke();
+}
+
+
+/**
+ * Tegn en trekant
+ * @param {CanvasRenderingContext2D} ctx
+ * @param {{ x: any; y: any; }} p1
+ * @param {{ x: any; y: any; }} p2
+ */
+function trekant(ctx, p1, p2) {
+  ctx.beginPath();
+  ctx.moveTo(p1.x, p1.y);
+  ctx.lineTo(p2.x, p2.y);
+  ctx.lineTo(p1.x, p2.y);
+  ctx.closePath()
+  ctx.stroke();
+}
+
+const p1 = {x:1,y:1};
+const p2 = {x:1,y:1};
+let antallPunkt = 0;
+
+
+/**
+ * @param {MouseEvent} e
+ */
+function registrerPunkt(e) {
+  p2.x = p1.x; p2.y = p1.y;
+  const { offsetX, offsetY } = e;
+  p1.x = offsetX;
+  p1.y = offsetY;
+  antallPunkt++;
+  if (antallPunkt === 2) {
+    const event = new Event('toPunkt');
+    dispatchEvent(event);
+    antallPunkt = 0;
+  }
+}
+
+
+
+
+function setup() {
+  const canvas = /** @type {HTMLCanvasElement} */
+    (document.getElementById("tegning"));
+  const ctx = canvas.getContext("2d");
+  // kobling til tegnings-kontekst for canvas ctx = context
+
+
+  canvas.addEventListener("click", registrerPunkt);
+
+
+  addEventListener("toPunkt", tegn);
+
+  function tegn() {
+    console.log("Tegner");
+    trekant(ctx,p1,p2);
+  }
+
+  //const p1 = { x: 100 + 10, y: 100 + 10 }
+  //const p2 = { x: 100 + 40, y: 100 + 50 }
+  const radius = dist(p1, p2);
+
+  sirkel(ctx, p1, radius);
+
+  // firkant(ctx,p1,p2);
+  trekant(ctx, p1, p2);
+
+
+
+
 
   // cursor position javascript
   /*
