b656ceb9f9f7e11dbeb1cc9ad41ddcb63b326d7d
Audun Hauge <audun.hauge@gmail.com>
2021-04-21 12:00:06 +0200

    tester

--- Test/debug.js
+++ Test/debug.js
@@ -14,9 +14,25 @@ function sqrt(x) {
 }
 
 function avg(arr) {
-    const s = arr.reduce((s,v)=> s+v,0);
-    return s/arr.length;
+   if (arr.length === 0) return 0;
+   const s = arr.reduce((s,v) => s+v,0);
+   return s/arr.length;  
 }
+
+
+function largest2(arr) {
+    const sorted = arr.sort((a,b) => b-a);
+    const [u,v] = sorted.slice(0,2);
+    return [v,u];
+}
+
+expect(largest2([1,2]),"largest[1,2]").is.a("Array");
+expect(largest2([1,2]),"largest[1,2]").to.have(0).eq(1);
+expect(largest2([1,2]),"largest[1,2]").to.have(1).eq(2);
+expect(largest2([1,2,3,4]),"largest[1,2,3,4]").to.have(0).eq(3);
+expect(largest2([1,2,-13,0,3.14]),"largest[...]").to.have(1).eq(3.14);
+expect(largest2([1,1,1,4,4]),"largest[...]").to.have(0).eq(4);
+expect(largest2([1,1,1,4,4]),"largest[...]").to.have(1).eq(4);
 
 expect(sqrt(2),"sqrt(2)").to.be(1);
 expect((sqrt(-1)).toString(),"sqrt(-1)").to.be("NaN");
@@ -24,7 +40,9 @@ expect(sqrt(-1),"sqrt(-1)").is.a("Number");
 
 expect(avg([1,1]),"avg[1,1]").to.be(1);
 expect(avg([]),"avg[]").to.be(0);
+expect(avg([-1,1]),"avg[-1,1]").to.be(0);
 
 
 
+expect(avg([1,2]),"avg[-1,1]").to.be(1.5);
 Test.summary("#Summary");
