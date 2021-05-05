6e9bf2c54025848bd9429422f69fa014ea6f9f62
Audun Hauge <audun.hauge@gmail.com>
2021-05-05 13:49:34 +0200

    løsning etter å ha lest oppgaveteksten

--- EksamenH2018/oppg3.js
+++ EksamenH2018/oppg3.js
@@ -1,5 +1,19 @@
 // @ts-check
 
+// etter å ha lest oppgaven ser jeg at jeg har bytta
+// rekkefølge på hytte/ferie
+// Skal velge ferie først, får da opp ledige hytter
+
+// Ser nå at vi trenger tabell 2, hadde ikke lest hele oppgaveteksten ....
+
+// Løsningen er mellomgod - bør egentlig lage begge
+//   selectene ut fra tabellene - slik at en ikke kan velge
+//   en sesong som er fullbooka.
+// Velger denne løsningen da den forekommer oftere blandt eksamenssettene
+// -- dvs opplagt lurt å lage den første select i html og bare den andre med kode
+// her bør begge kodes
+
+
 /**
  * Kortform for document.getElementById
  * @param {string} id 
@@ -14,34 +28,76 @@ const $ = id => document.getElementById(id);
  */
 const getValue = elm => elm.value
 
+const VELG = "...velg...";
+
 const utleie = {
-    Granstua: ["Påske"],
-    Granbo: ["Jul","Vinterferie"],
-    Grantoppe: [],
-    Granhaug: [],
+    Jul:["Granbo"],
+    Påske:["Granstua"],
+    Vinterferie:["Granbo","Grantoppen","Granhaug"],
+}
+
+const hytter = {
+    Granstua: {senger:4,standard:"høy",badstu:true,pris:12,bilde:""},
+    Granbo: {senger:6,standard:"middels",badstu:false,pris:15,bilde:""},
+    Grantoppen: {senger:8,standard:"lav",badstu:false,pris:16,bilde:""},
+    Granhaug:{senger:10,standard:"høy",badstu:true,pris:30,bilde:""},
 }
 
 function setup() {
-    const selHytte = $("hyttesel");
-    const divFerie = $("ferie");
+    const selFerie = $("feriesel");
+    const divHytte = $("hytte");
+    const btnLagre = $("lagre");
+    const divInfo = $("hytteinfo");
 
-    selHytte.addEventListener("change", viseFerie);
+    selFerie.addEventListener("change", visHytte);
+    btnLagre.addEventListener("click", lagreBestilling);
 
-    function viseFerie() {
-        const hytte = getValue(selHytte);
-        const liste = utleie[hytte];
+    function visHytte() {
+        const ferie = getValue(selFerie);
+        const liste = utleie[ferie];
+        divInfo.innerHTML = "";
         if (liste) {
             const sel = makeSelect(liste);
-            divFerie.innerHTML = "";
-            divFerie.append(sel);
+            divHytte.innerHTML = "";
+            divHytte.append(sel);
+            sel.addEventListener("change",visinfo);
+            function visinfo() {
+                const hytte = getValue(sel);
+                const info = hytter[hytte];
+                if (info) {
+                    divInfo.innerHTML = `${hytte} senger ${info.senger}`
+                }
+            }
+        }
+
+        
+    }
+
+    function lagreBestilling() {
+        const hytte = getValue( document.querySelector("#hytte select"));
+        const ferie = getValue( selFerie);
+        if (hytte && ferie) {  // denne trengs kanskje ikke
+            if (hytte !== VELG && ferie !== VELG) {
+                const ledige = new Set(utleie[ferie]);
+                ledige.delete(hytte);
+                utleie[ferie] = Array.from(ledige);
+                selFerie.selectedIndex = 0;
+                divHytte.innerHTML = "";
+                divInfo.innerHTML = "";
+            }
         }
     }
 
 }
 
+/**
+ * Lager en select fra en array med tekst
+ * @param {Array<string>} liste 
+ * @returns {HTMLSelectElement}
+ */
 function makeSelect(liste) {
     const sel = document.createElement("select");
-    let s = "";
+    let s = "<option>" + VELG + "</option>";
     for (const opt of liste) {
         s += `<option>${opt}</option>`;
     }
